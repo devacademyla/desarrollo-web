7f983101f56d8a34d3e5760ed26de992e23d06a3
brodriguezaybar <brodriguezaybar@gmail.com>
2015-09-03 09:52:31 -0500

    Código final clase 3

--- clase-3/devflix/javascript/app.js
+++ clase-3/devflix/javascript/app.js
@@ -4,7 +4,39 @@ angular.module("Devflix", []);
 // LLamando un módulo para crearle un controller
 angular.module("Devflix").controller("SerieController", SerieController);
 
+angular.module("Devflix").controller("MainController", MainController);
+
 // Constructor
 function SerieController() {
-  this.mensaje = "Hola RCP!";
+  this.pelicula = {
+    descripcion: "Bla bla bla bla bla",
+    estreno: 2015,
+    tag: "NR",
+    duracion: "1h 24m",
+    titulo: "Ouija"
+  };
 }
+
+function MainController() {
+  this.anadido = false;
+  this.anadir = function () {
+    this.anadido = true;
+  }
+
+  this.agregados = [
+    "bates-motel",
+    "better-call-saul",
+    "breaking-bad",
+    "dexter",
+    "downton-abbey",
+    "dr-house"
+  ];
+}
+
+
+
+
+
+
+
+
